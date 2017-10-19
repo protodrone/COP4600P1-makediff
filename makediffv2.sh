e09b9ef7003e7125007cc7b193f2fc148267cd62
Warren Brown <protodrone@gmail.com>
2017-10-19 17:22:58 -0400

    initial commit

---
+++ makediffv2.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+TEMPFILES="sedtemp.txt newfiles.txt newfilestemp.txt existingfilesorig.txt existingfiles.txt existfilediffstub.txt"
+
+if [ -e p1diff.txt ]
+then
+	rm p1diff.txt
+else
+	touch p1diff.txt
+fi
+
+rmTempFiles()
+ {
+	for fileName in $TEMPFILES
+	do
+		if [ -e $fileName ]
+		then
+			rm $fileName
+		fi 
+	done
+}
+
+rmTempFiles
+
+sed 's/src/src_original/' filelist.txt > sedtemp.txt
+
+while read f; do
+	
+	if [ -e $f ]
+	then
+		echo $f >> existingfilesorig.txt 
+	else
+		echo $f >> newfilestemp.txt
+	fi
+done <sedtemp.txt
+
+sed 's/src_original/src/' newfilestemp.txt > newfiles.txt
+sed 's/src_original/src/' existingfilesorig.txt > existingfiles.txt
+paste existingfilesorig.txt existingfiles.txt > existfilediffstub.txt
+
+while read j; do
+#	if [ -z $j ]
+#	then
+#		echo "Skipping empty line."
+#	else
+		echo "diff -uN $j >> p1diff.txt"
+        	diff -uN $j >> p1diff.txt
+#	fi
+done <existfilediffstub.txt
+
+while read k; do
+	if [ -z $k ]
+	then
+		echo "Skipping empty line."
+	else
+		echo "diff -u --unidirectional-new-file /dev/null $k >> p1diff.txt"
+		diff -u --unidirectional-new-file /dev/null $k >> p1diff.txt
+	fi
+done <newfiles.txt
+
+rmTempFiles
+
+
+
+
+
